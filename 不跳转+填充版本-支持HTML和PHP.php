33bdc7fd7f5b85f69ee4228856ab84c24b257e12
BlueSkyXN <xienan0714@gmail.com>
2021-02-16 13:21:19 +0800

    Create 不跳转+填充版本-支持HTML和PHP.php

---
+++ 不跳转+填充版本-支持HTML和PHP.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+<title>BlueSkyXN/RandomIMGAPI</title>
+</head>
+<body background="https://www.yunboys.cn/sjbz/api.php?method=mobile&lx=dongman"
+style=" background-repeat:no-repeat ;
+background-size:100% 100%; 
+background-attachment: fixed;" >
+</body>
+</html>
